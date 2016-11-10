d9af8d8930ee1bdb585cb4cd9181113a4eb1425d
brianxautumn <shred@brianautumn.com>
2016-11-09 20:49:18 -0800

    Starting automated testing

--- test/test.js
+++ test/test.js
@@ -9,65 +9,56 @@ var fs = require('fs');
 var decoder = new vp8();
 var ivf = new ivf();
 var vectorPath = 'vp8-test-vectors/';
+var testData = require('./test-data.json');
+
+
+for(var file in testData){
+    console.log(file);
+}
+
 var data;
 
-describe('Load', function () {    
 
-    describe('Loaded Vp8 Decoder', function () {
-        it('Should Initialize decoder', function () {
-            data = fs.readFileSync(vectorPath + 'vp80-00-comprehensive-001.ivf');
+
+describe('Running Test Vectors', function () {
+    for (var file in testData) {
+        var frameData = testData[file];
+        var sz;
+
+        describe('Loading Vector : ' + file, function () {
+            data = fs.readFileSync(vectorPath + file);
             ivf.receiveBuffer(data);
             ivf.parseHeader();
 
-            assert.ok(decoder);
-            assert.ok(ivf);
+
+            for (var i = 0; i < frameData.length; i++) {
+                var valid = frameData[i];
+                var data = new Uint8Array(ivf.processFrame()); //frame data
+                var sz = data.byteLength;
+                var res;
+                decoder.saved_entropy_valid = 0;
+
+
+                it('Parse frame header ' + i, function () {
+                    if ((res = decoder.frame_hdr.parse(data, sz)))
+                        throw "Failed to parse frame header";
+
+                    assert.equal(decoder.frame_hdr.is_keyframe, valid.is_keyframe);
+                    assert.equal(decoder.frame_hdr.version, valid.version);
+                    assert.equal(decoder.frame_hdr.is_shown, valid.is_shown);
+
+                    if (valid.is_keyframe) {
+                        assert.equal(decoder.frame_hdr.kf.w, valid.w);
+                        assert.equal(decoder.frame_hdr.kf.h, valid.h);
+                        assert.equal(decoder.frame_hdr.kf.scale_w, valid.scale_w);
+                        assert.equal(decoder.frame_hdr.kf.scale_h, valid.scale_h);
+                    }
+
+                });
+            }
+
         });
-    });
-    
+    }
+
+
 });
-
-describe('Process', function () {    
-    var sz;
-    describe('Vector 1', function () {
-        
-        it('Parse frame 1 header', function () {
-            var data = new Uint8Array(ivf.processFrame()); //frame #1
-            var sz = data.byteLength;
-            var res;
-            decoder.saved_entropy_valid = 0;
-
-            if ((res = decoder.frame_hdr.parse(data, sz)))
-                throw "Failed to parse frame header";
-
-            assert.equal(decoder.frame_hdr.is_keyframe, true);
-            //assert.equal(decoder.frame_hdr.is_experimental, 0);
-            assert.equal(decoder.frame_hdr.version, 0);
-            assert.equal(decoder.frame_hdr.is_shown, 1);
-            assert.equal(decoder.frame_hdr.kf.w, 176);
-            assert.equal(decoder.frame_hdr.kf.h, 144);
-            assert.equal(decoder.frame_hdr.kf.scale_w, 0);
-            assert.equal(decoder.frame_hdr.kf.scale_h, 0);
-        });
-        
-        it('Parse frame 2 header', function () {
-            var data = new Uint8Array(ivf.processFrame()); //frame #1
-            var sz = data.byteLength;
-            var res;
-            decoder.saved_entropy_valid = 0;
-
-            if ((res = decoder.frame_hdr.parse(data, sz)))
-                throw "Failed to parse frame header";
-
-            assert.equal(decoder.frame_hdr.is_keyframe, false);
-            //assert.equal(decoder.frame_hdr.is_experimental, 0);
-            assert.equal(decoder.frame_hdr.version, 0);
-            assert.equal(decoder.frame_hdr.is_shown, 1);
-
-        });
-        
-        
-        
-    });
-    
-});
-
